4aa6cf9fee533451203696a6a4a2afaded7ba51f
cowe <cowe@tzi.de>
2016-11-11 21:09:17 +0100

    :rocket: rename package and setup analasis engine

---
+++ percepteros/cmake/percepteROS_config.cmake
@@ -0,0 +1,6 @@
+set(percepteROS_NAMESPACE           percepteROS)
+set(percepteROS_TYPESYSTEM_CPP_PATH /home/surxz/caterros/src/perception/percepteROS/include/percepteROS/types)
+set(percepteROS_TYPESYSTEM_XML_PATH /home/surxz/caterros/src/perception/percepteROS/descriptors/typesystem)
+set(percepteROS_ANNOTATOR_PATH      /home/surxz/caterros/src/perception/percepteROS/descriptors/annotators)
+set(percepteROS_ENGINE_PATH         /home/surxz/caterros/src/perception/percepteROS/descriptors/analysis_engines)
+
