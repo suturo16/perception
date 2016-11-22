3ba58279a23560e7f655638d250f3862989e226f
cowe <cowe@tzi.de>
2016-11-22 19:21:35 +0100

    :muscle: remove config

--- percepteros/cmake/percepteros_config.cmake
+++
@@ -1,6 +0,0 @@
-set(percepteros_NAMESPACE           percepteROS)
-set(percepteros_TYPESYSTEM_CPP_PATH /home/tobias/ros/cateros/src/perception/percepteros/include/percepteROS/types)
-set(percepteros_TYPESYSTEM_XML_PATH /home/tobias/ros/cateros/src/perception/percepteros/descriptors/typesystem)
-set(percepteros_ANNOTATOR_PATH      /home/tobias/ros/cateros/src/perception/percepteros/descriptors/annotators)
-set(percepteros_ENGINE_PATH         /home/tobias/ros/cateros/src/perception/percepteros/descriptors/analysis_engines)
-
