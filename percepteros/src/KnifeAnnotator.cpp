433466033bbfa06493f96ab0175c1cadd0eb9370
Tobias Hahn <morowinger@gmail.com>
2017-02-16 02:22:07 +0100

    added reading

--- percepteros/src/KnifeAnnotator.cpp
+++ percepteros/src/KnifeAnnotator.cpp
@@ -63,10 +63,10 @@ public:
   }
 
 	pcl::PointCloud<PointG>::Ptr loadPly(const char* filename) {
-		pcl::PointCloud<PointG>::Ptr model = pcl::PointCloud<PointG>::Ptr(pcl::PointCloud<PointG>);
+		pcl::PointCloud<PointG>::Ptr model = pcl::PointCloud<PointG>::Ptr(new pcl::PointCloud<PointG>);
 		int numVertices = 0;
 
-		std::ifstream ifs(fileName);
+		std::ifstream ifs(filename);
 		
 		//check if file available
 		if(!ifs.is_open()) {
@@ -86,8 +86,37 @@ public:
 
 		//get data of points
 		for (int i = 0; i < numVertices; i++) {
+			float data[6];
+			PointG* point = new PointG;
 			
+			//get raw data
+			for (int j = 0; j < 6; j++) {
+				std::getline(ifs, str, ' ');
+				data[j] = stof(str);
+			}
+			std::getline(ifs, str);
+
+			//normalize normals
+			double norm = sqrt(data[3]*data[3] + data[4]*data[4] + data[5]*data[5]);
+			if (norm > 0.00001) {
+				data[3] /= (float)norm;
+				data[4] /= (float)norm;
+				data[5] /= (float)norm;
+			}
+			
+			//setting point
+			point->x = data[0];
+			point->y = data[1];
+			point->z = data[2];
+			point->normal_x = data[3];
+			point->normal_y = data[4];
+			point->normal_z = data[5];
+			
+			//push point
+			model->push_back(*point);
 		}
+
+		return model;
 	}
 };
 
