8c3dc4f8fe6d6654fbfe4932e2698732dcc6fd2f
Tobias Hahn <morowinger@gmail.com>
2017-02-27 15:50:39 +0100

    preparing for merge

--- publisher/src/publisher.cpp
+++ publisher/src/publisher.cpp
@@ -5,7 +5,6 @@
 #include <tf/transform_broadcaster.h>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "visualization_msgs/Marker.h"
-#include <iostream>
 
 ros::Publisher vis_pub1;
 ros::Publisher vis_pub2;
@@ -17,7 +16,8 @@ tf::TransformBroadcaster* br;
  */
 void subscriber(const suturo_perception_msgs::ObjectDetection& msg)
 {
-    if(msg.type==6){
+
+    if(msg.type==2){
       geometry_msgs::PoseStamped pose;
 
       pose = msg.pose;
@@ -50,37 +50,56 @@ void subscriber(const suturo_perception_msgs::ObjectDetection& msg)
       marker.color.a = 1.0;
       std::cout<<"Publishing";
 
+      visualization_msgs::Marker markerCyl;
+      markerCyl.header = pose.header;
+      markerCyl.id=1;
+      markerCyl.type = visualization_msgs::Marker::CYLINDER;
+      markerCyl.ns = "percepteros";
+      markerCyl.pose = pose.pose;
+      markerCyl.scale.x = msg.width;
+      markerCyl.scale.y = msg.depth;
+      markerCyl.scale.z = msg.height;
+      // Set the color -- be sure to set alpha to something non-zero!
+      markerCyl.color.r = 1.0f;
+      markerCyl.color.g = 1.0f;
+      markerCyl.color.b = 0.0f;
+      markerCyl.color.a = 1.0;
+
+
       vis_pub1.publish(marker);
-    } /*else if (msg.type==5) {
-    	geometry_msgs::PoseStamped pose;
+      vis_pub2.publish(markerCyl);
+    } else if (msg.type==6) {
+      geometry_msgs::PoseStamped pose;
 
-      	pose = msg.pose;
+      pose = msg.pose;
 
-      	tf::TransformBroadcaster br;
+      tf::Stamped<tf::Pose> transform;
 
-      	tf::Stamped<tf::Pose> transform;
+      tf::poseStampedMsgToTF(pose, transform);
 
-      	tf::poseStampedMsgToTF(pose, transform);
+      br->sendTransform(tf::StampedTransform(transform, ros::Time::now(), pose.header.frame_id, msg.name));
 
-      	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "head_mount_kinect_rgb_optical_frame", msg.name));
+      visualization_msgs::Marker marker;
+      marker.header = pose.header;
+      marker.ns = "percepteros";
+      marker.id = 0;
+      marker.type = visualization_msgs::Marker::ARROW;
+      marker.action = visualization_msgs::Marker::ADD;
+      marker.pose = pose.pose;
+      // Set the scale of the marker -- 1x1x1 here means 1m on a side
+      marker.scale.x = 0.1;
+      marker.scale.y = 0.01;
+      marker.scale.z = 0.01;
 
-     	visualization_msgs::Marker markerTray;
-      	markerTray.header = pose.header;
-      	markerTray.id=2;
-      	markerTray.type = visualization_msgs::Marker::CUBE;
-      	markerTray.ns = "percepteros";
-      	markerTray.pose = pose.pose;
-      	markerTray.scale.x = msg.width;
-      	markerTray.scale.y = msg.depth;
-      	markerTray.scale.z = 0.1;
-      	// Set the color -- be sure to set alpha to something non-zero!
-      	markerTray.color.r = 1.0f;
-      	markerTray.color.g = 1.0f;
-      	markerTray.color.b = 0.0f;
-      	markerTray.color.a = 1.0;
+      // Set the color -- be sure to set alpha to something non-zero!
+      marker.color.r = 0.0f;
+      marker.color.g = 1.0f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0;
+      std::cout<<"Publishing";
 
-      	vis_pub2.publish(markerTray);
-    }*/
+      vis_pub1.publish(marker);
+    }
 }
 
 int main(int argc, char **argv)
