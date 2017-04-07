ef5c0f69b390cb66381808cd563bd722f5966446
Tobias Hahn <morowinger@gmail.com>
2017-04-07 10:05:29 +0200

    :muscle: fixed tray perception

--- percepteros/src/TrayAnnotator.cpp
+++ percepteros/src/TrayAnnotator.cpp
@@ -34,6 +34,7 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/common/pca.h>
 
 // OTHER
 #include <percepteros/types/all_types.h>
@@ -43,19 +44,17 @@ using namespace uima;
 
 typedef pcl::PointXYZRGBA PointR;
 typedef pcl::PointCloud<PointR> PCR;
-typedef pcl::Normal N;
-typedef pcl::PointCloud<N> PN;
-typedef pcl::PointNormal PointN;
-typedef pcl::PointCloud<PointN> PCN;
 
 class TrayAnnotator : public DrawingAnnotator
 {
 
 private:
-	PCR::Ptr temp = PCR::Ptr(new PCR);
-	PN::Ptr tempN = PN::Ptr(new PN);
-	PCN::Ptr cloud = PCN::Ptr(new PCN);
-	PCN::Ptr tray = PCN::Ptr(new PCN);
+	PCR::Ptr cloud = PCR::Ptr(new PCR);
+	PCR::Ptr tray = PCR::Ptr(new PCR);
+	PCR::Ptr proj = PCR::Ptr(new PCR);
+
+	tf::Matrix3x3 rot;
+	tf::Vector3 origin;
 
 public:
   TrayAnnotator() : DrawingAnnotator(__func__)
@@ -87,25 +86,17 @@ private:
     rs::SceneCas cas(tcas);
     rs::Scene scene = cas.getScene();
 
-    cas.get(VIEW_CLOUD, *temp);
-		cas.get(VIEW_NORMALS, *tempN);
-		pcl::copyPointCloud(*temp, *cloud);
-		pcl::copyPointCloud(*tempN, *cloud);
+    cas.get(VIEW_CLOUD, *cloud);
 
 		std::vector<rs::Cluster> clusters;
 		scene.identifiables.filter(clusters);
-		
-		pcl::ModelCoefficients coefficients;
-		pcl::PointIndices inliers;
 
-		pcl::SACSegmentationFromNormals<PointN, PointN> seg;
-		seg.setModelType(pcl::SACMODEL_NORMAL_PLANE);
+		pcl::SACSegmentation<PointR> seg;
+		seg.setModelType(pcl::SACMODEL_PLANE);
 		seg.setMethodType(pcl::SAC_RANSAC);
-		seg.setMaxIterations(2000);
 		seg.setDistanceThreshold(0.01);
-		seg.setAxis(Eigen::Vector3f(1,0,0));
-		seg.setEpsAngle(0.1);
-		seg.setOptimizeCoefficients(true);
+	
+		pcl::PCA<PointR> pca; 
 
 		for (auto cluster : clusters) {
 			if (cluster.source.get().compare(0, 13, "HueClustering") == 0) {
@@ -115,24 +106,109 @@ private:
 
 				extractPoints(cloud, tray, cluster_indices);
 
+				pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+				pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+
 				seg.setInputCloud(tray);
-				seg.setInputNormals(tray);
-				seg.segment(inliers, coefficients);
+				seg.segment(*inliers, *coefficients);
 				
-				outInfo(inliers.indices.size() / tray->points.size());
-				if (inliers.indices.size() / tray->points.size() > 0.9) {
-					outInfo(cluster.source.get());
+				if (inliers->indices.size() / tray->points.size() > 0.9) {
+					float hue = std::strtof(cluster.source.get().substr(15).data(), NULL);
+
+					if (hue < 10 || hue > 320 || (hue > 40 && hue < 80)) {
+						pca.setInputCloud(tray);
+
+						auto ev = pca.getEigenVectors();
+
+						percepteros::RecognitionObject o = rs::create<percepteros::RecognitionObject>(tcas);
+						rs::PoseAnnotation poseA = rs::create<rs::PoseAnnotation>(tcas);
+						tf::StampedTransform camToWorld;
+						if (scene.viewPoint.has()) {
+							rs::conversion::from(scene.viewPoint.get(), camToWorld);
+						}
+
+						tf::Transform transform;
+						origin = getOrigin(tray);
+						transform.setOrigin(getOrigin(tray));
+
+						rot.setValue(ev(0,0), ev(0,1), ev(0,2), ev(1,0), ev(1,1), ev(1,2), ev(2,0), ev(2,1), ev(2,2));
+						transform.setBasis(rot);
+
+						o.name.set("DropZone");
+						o.type.set(5);
+						o.width.set(0.21);
+						o.height.set(0.29);
+						o.depth.set(0);
+						
+						tf::Stamped<tf::Pose> camera(transform, camToWorld.stamp_, camToWorld.child_frame_id_);
+						tf::Stamped<tf::Pose> world(camToWorld * transform, camToWorld.stamp_, camToWorld.frame_id_);
+
+						poseA.source.set("TrayAnnotator");
+						poseA.camera.set(rs::conversion::to(tcas, camera));
+						poseA.world.set(rs::conversion::to(tcas, world));
+
+						cluster.annotations.append(poseA);
+						cluster.annotations.append(o);
+					}
 				}
 			}
 		}
 		return UIMA_ERR_NONE;
   }
 
-	void extractPoints(PCN::Ptr cloud, PCN::Ptr object, pcl::PointIndices::Ptr indices) {
+	void extractPoints(PCR::Ptr cloud, PCR::Ptr object, pcl::PointIndices::Ptr indices) {
 		object->clear();
 		for (size_t i = 0; i < indices->indices.size(); i++) {
-			object->push_back(cloud->points[i]);
+			object->push_back(cloud->points[indices->indices[i]]);
 		}
+	}
+
+	tf::Vector3 getOrigin(PCR::Ptr tray) {
+		tf::Vector3 origin(0, 0, 0);
+		int size = tray->points.size();
+
+		for (int i = 0; i < tray->points.size(); i++) {
+			origin[0] += tray->points[i].x / size;
+			origin[1] += tray->points[i].y / size;
+			origin[2] += tray->points[i].z / size;
+		}
+
+		return origin;
+	}
+
+	void fillVisualizerWithLock(pcl::visualization::PCLVisualizer &vis, const bool firstRun) {
+		if (firstRun) {
+			vis.addPointCloud(cloud, "scene points");
+		} else {
+			vis.updatePointCloud(cloud, "scene points");
+			vis.removeAllShapes();
+		}
+
+		vis.addCone(getCoefficients(0, rot, origin), "x");
+		vis.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "x");
+
+
+		vis.addCone(getCoefficients(1, rot, origin), "y");
+		vis.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0, 1, 0, "y");
+
+		vis.addCone(getCoefficients(2, rot, origin), "z");
+		vis.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0, 0, 1, "z");
+	}
+
+	pcl::ModelCoefficients getCoefficients(int start, tf::Matrix3x3 rot, tf::Vector3 origin) {
+		pcl::ModelCoefficients co;
+
+		co.values.push_back(origin[0]);
+		co.values.push_back(origin[1]);
+		co.values.push_back(origin[2]);
+
+		co.values.push_back(rot.getRow(start).getX());
+		co.values.push_back(rot.getRow(start).getY());
+		co.values.push_back(rot.getRow(start).getZ());
+
+		co.values.push_back(1.0f);
+
+		return co;
 	}
 };
 
